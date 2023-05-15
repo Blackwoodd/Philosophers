b4a161e9efeccc777afbf09ad5964d74f6fc544f
Blackwood <107251262+Blackwoodd@users.noreply.github.com>
2023-05-15 15:22:26 +0200

    Update philo_eat.c

--- philo/src/philo_eat.c
+++ philo/src/philo_eat.c
@@ -1,16 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo_eat.c                                        :+:      :+:    :+:   */
+/*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nbechon <nbechon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 14:39:36 by nbechon           #+#    #+#             */
-/*   Updated: 2023/05/09 15:23:57 by nbechon          ###   ########.fr       */
+/*   Updated: 2023/05/12 16:06:03 by nbechon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
+
+void	*count_meal(void *data)
+{
+	int		i;
+	int		last_eat;
+	t_philo	*philo;
+
+	philo = (t_philo *)data;
+	usleep(60000);
+	while (exit_check(philo) == false)
+	{
+		i = 0;
+		last_eat = 0;
+		while (i < philo->data->number_of_philosophers)
+		{
+			pthread_mutex_lock(&philo->data->mutex[READ]);
+			if (philo[i].eat_times >= philo->data->must_eat)
+				last_eat++;
+			pthread_mutex_unlock(&philo->data->mutex[READ]);
+			i++;
+		}
+		usleep(100);
+		if (last_eat == philo->data->number_of_philosophers)
+			return (kill_philo(philo), NULL);
+	}
+	return (EXIT_SUCCESS);
+}
 
 static void	choosing_fork(t_philo *philo, int order)
 {
@@ -37,16 +64,43 @@ int	ft_eating(t_philo *philo)
 	if (philo->data->number_of_philosophers == 1)
 	{
 		pthread_mutex_unlock(&philo->forks[philo->right_fork]);
-		return (EXIT_SUCCESS);
+		return (EXIT_FAILURE);
 	}
 	choosing_fork(philo, 2);
 	print_action(philo, FORK);
 	print_action(philo, EAT);
 	pthread_mutex_lock(&philo->data->mutex[READ]);
+	if (philo->eat_times != -1)
+		philo->eat_times++;
 	philo->last_meal = get_time();
 	pthread_mutex_unlock(&philo->data->mutex[READ]);
-	for_wait(philo->data->time_to_eat);
+	for_wait(philo->data->time_to_eat, philo);
 	pthread_mutex_unlock(&philo->forks[philo->left_fork]);
 	pthread_mutex_unlock(&philo->forks[philo->right_fork]);
-	return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
+
+void	*life_philosopher(void *arg)
+{
+	t_philo			*philo;
+
+	philo = (t_philo *)arg;
+	if ((philo->id % 2) == 0)
+	{
+		print_action(philo, THINK);
+		usleep(60000);
+	}
+	while (exit_check(philo) == false)
+	{
+		ft_eating(philo);
+		print_action(philo, SLEEP);
+		for_wait(philo->data->time_to_sleep, philo);
+		if (philo->data->number_of_philosophers % 2 == 1)
+			usleep(1000);
+		else
+			usleep(100);
+		print_action(philo, THINK);
+		for_wait(philo->data->time_to_think, philo);
+	}
+	return (0);
 }
