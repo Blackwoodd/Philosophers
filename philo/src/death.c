2f8f6ee60640988a52aab37628cf8bb2a099ac36
Blackwood <107251262+Blackwoodd@users.noreply.github.com>
2023-05-15 15:24:18 +0200

    Update death_exit.c

--- philo/src/death.c
+++ philo/src/death.c
@@ -1,16 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   death.c                                            :+:      :+:    :+:   */
+/*   death_exit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nbechon <nbechon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 14:01:06 by nbechon           #+#    #+#             */
-/*   Updated: 2023/05/09 15:31:34 by nbechon          ###   ########.fr       */
+/*   Updated: 2023/05/15 15:24:02 by nbechon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
+
+int	exit_check(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->data->mutex[EXIT]);
+	if (philo->data->should_exit == true)
+		return (pthread_mutex_unlock(&philo->data->mutex[EXIT]), true);
+	return (pthread_mutex_unlock(&philo->data->mutex[EXIT]), false);
+}
 
 int	someone_starved(t_philo *philo, u_int64_t time_to_die)
 {
@@ -39,10 +47,10 @@ void	*death(void *arg)
 			pthread_mutex_lock(&philo->data->mutex[READ]);
 			if (someone_starved(&philo[i], philo[i].data->time_to_die))
 			{
-				printf("%lu %i died\n", timestamp(philo->data->time), philo->id);
 				pthread_mutex_unlock(&philo->data->mutex[READ]);
 				kill_philo(philo);
-				return (death(&philo[i]), NULL);
+				printf("%lu %i died\n", timestamp(philo->data->time), philo->id);
+				return (NULL);
 			}
 			pthread_mutex_unlock(&philo->data->mutex[READ]);
 			i++;
