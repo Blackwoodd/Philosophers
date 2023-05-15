e58ae6a3381902efe6e23bf5dab3b966dee39263
Blackwood <107251262+Blackwoodd@users.noreply.github.com>
2023-05-15 15:23:01 +0200

    Update mutex.c

--- philo/src/mutex.c
+++ philo/src/mutex.c
@@ -6,21 +6,20 @@
 /*   By: nbechon <nbechon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 12:35:21 by nbechon           #+#    #+#             */
-/*   Updated: 2023/05/09 16:08:23 by nbechon          ###   ########.fr       */
+/*   Updated: 2023/05/12 14:37:10 by nbechon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
-int	destroy_mutex(t_philo *philo)
+void	destroy_mutex(t_philo *philo)
 {
 	int	i;
 
-	i = 0;
-	while (i < philo->data->number_of_philosophers)
-	{
+	i = -1;
+	while (++i < philo->data->number_of_philosophers)
+		pthread_mutex_destroy(&philo->forks[i]);
+	i = -1;
+	while (++i < M_NUM)
 		pthread_mutex_destroy(&philo->data->mutex[i]);
-		i++;
-	}
-	return (TRUE);
 }
